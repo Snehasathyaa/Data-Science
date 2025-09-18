f6508220ede47ca9d0e2dfc53cf849ef1b0adcca
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2025-09-18 15:48:55 +0530

    Create student_kNN.R

---
+++ R program/student_kNN.R
@@ -0,0 +1,16 @@
+library(class)
+survey=read.csv("C:/Users/Admin/Desktop/Sneha - Ds/file/survey.csv")
+student=data.frame(X=5,Y=7)
+survey1=survey[,1:2]
+pred=knn(survey1,student,survey$Z,k=3)
+cat("Predicted Classification of Student:\n")
+print(pred)
+
+
+
+---------------------------------------------------
+
+Predicted Classification of Student:
+> print(pred)
+[1] Outstanding
+Levels: Good Outstanding
